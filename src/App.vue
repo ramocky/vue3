51d87a3f48ddffff75ac92d3f5e7100dab741650
zlf <519419249@qq.com>
2021-04-30 17:26:34 +0800

    ci(插件):

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,4 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-    |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view />
 </template>
 
@@ -13,7 +8,7 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50
+  color: #2c3e50;
 }
 
 #nav {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <button @click="inc">{{ userName }}</button>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  setup() {
+    const userName = ref('无名')
+    const inc = () => userName.value = '测试'
+    return {
+      userName,
+      inc
+    }
   }
 }
 </script>
