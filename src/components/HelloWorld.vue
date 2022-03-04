9a94603b6508155583e4f0c3c2ab4e89f5e8951f
519419249@qq.com <zlf.13123097128>
2022-03-04 15:41:59 +0800

    vue3 setup init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <input
+    type="text"
+    v-model="myName"
+    @click="handleEdit"
+    class="search-input-border"
+    @change="testEmit"
+  />
+  {{ user }}
+  {{ msg }}
+</template>
+<script setup>
+import { computed, defineEmits, defineProps, ref, toRefs } from "vue";
+import { reactive } from "vue";
+defineProps({
+  msg: String,
+});
+const emit = defineEmits(["test"]);
+const myName = ref("test");
+const myObj = reactive({
+  name: "sali",
+  age: "18",
+});
+const { name, age } = toRefs(myObj);
+const handleEdit = () => {
+  age.value = "22";
+  name.value = "测试";
+};
+const user = computed(() => {
+  return `年龄${age.value},名字${myName.value}`;
+});
+const testEmit = () => {
+  emit("test", "test-emit");
+};
+</script>
+<style>
+.search-input-border {
+  border:1px solid red;
+}
+</style>
---
+++ src/views/test.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>我是父组件</div>
+  <hello-world :msg=propsMsg @test="test"></hello-world>
+</template>
+<script setup>
+import HelloWorld from '../components/HelloWorld.vue'
+import {ref} from "vue";
+function test(val){
+  console.log(val)
+}
+const propsMsg=ref('子组件')
+</script>
+<style></style>
